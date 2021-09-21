d34c5fb2742d4b5fc4d8f5d5507b0b05184eb262
Adrian Favill <favill42@gmail.com>
2021-09-21 19:30:40 +0100

    Exit (error) codes 1 and "Z" should really be treated as warnings and a PDF will have been generated - see https://github.com/wkhtmltopdf/wkhtmltopdf/issues/1502#issuecomment-35705833.

--- utils/wkhtmltopdf.js
+++ utils/wkhtmltopdf.js
@@ -9,7 +9,26 @@ module.exports = function (html, options = []) {
             reject(error);
         }).on("exit", code => {
             if (code) {
-                reject(new Error(`wkhtmltopdf process exited with code ${code}`));
+				/*
+					https://github.com/wkhtmltopdf/wkhtmltopdf/issues/1502#issuecomment-35705833
+					
+					0	All OK
+					1	PDF generated OK, but some request(s) did not return HTTP 200
+					2	Could not something something
+					X	Could not write PDF: File in use
+					Y	Could not write PDF: No write permission
+					Z	PDF generated OK, but some JavaScript requests(s) timeouted
+					A	Invalid arguments provided
+					B	Could not find input file(s)
+					C	Process timeout
+				*/
+				
+				if (code === 1 || code === "Z") {
+					resolve(Buffer.concat(bufs));
+				}
+				else {
+					reject(new Error(`wkhtmltopdf process exited with code ${code}`));
+				}
             } else {
                 resolve(Buffer.concat(bufs));
             }
